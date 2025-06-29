910e439b6cfa680d617a2947a3850e572a55243e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-29 11:30:38 +0530

    code

--- src/Task2/StudentGradeCalculator.java
+++ src/Task2/StudentGradeCalculator.java
@@ -7,6 +7,20 @@ public class StudentGradeCalculator {
         System.out.println("\n💖Welcome to Student Grade Calculator Program 💖");
         float math , physics , chemistry , computerScience , english ;
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter Maths Number : ");
+        math = input.nextFloat();
+        System.out.print("Enter Physics Number : ");
+        physics = input.nextFloat();
+        System.out.print("Enter Chemistry Number : ");
+        chemistry = input.nextFloat();
+        System.out.print("Enter ComputerScience Number : ");
+        computerScience = input.nextFloat();
+        System.out.print("Enter English Number : ");
+        english = input.nextFloat();
+        float totalMarks = math + physics + chemistry + computerScience + english;
+        float averagePercentage = totalMarks/5;
+
+
 
     }
 }
