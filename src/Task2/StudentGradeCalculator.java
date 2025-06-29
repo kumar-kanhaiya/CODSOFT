c8ae8c51f9208eb79b47830fc9923de5f6425afd
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-29 11:26:22 +0530

    code

--- src/Task2/StudentGradeCalculator.java
+++ src/Task2/StudentGradeCalculator.java
@@ -1,8 +1,12 @@
 package Task2;
 
+import java.util.Scanner;
+
 public class StudentGradeCalculator {
     public static void main(String[] args) {
         System.out.println("\n💖Welcome to Student Grade Calculator Program 💖");
+        float math , physics , chemistry , computerScience , english ;
+        Scanner input = new Scanner(System.in);
 
     }
 }
